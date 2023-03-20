72c65841c0a119b8b5f7dac5e80915686b18d87a
Fritz Gabler <fgabler@1-A-4.42heilbronn.de>
2023-03-20 17:44:20 +0100

    version05

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,43 +6,52 @@
 /*   By: fgabler <fgabler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 13:23:41 by fgabler           #+#    #+#             */
-/*   Updated: 2023/03/20 13:57:42 by fgabler          ###   ########.fr       */
+/*   Updated: 2023/03/20 17:40:48 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
 
+size_t ft_strlen(const char *str);
+
 char *ft_strnstr(const char *big, const char *little, size_t len)
 {
-	int i;
-	char *tmp1;
-	char *tmp2;
+	size_t i;
+	size_t j;
 
-	tmp1 = (char *) big;
-	tmp2 = (char *) little;
 	i = 0;
-	if (little == (void *) '\0')
-	{
-		return (tmp1);
-	}
+	j = 0;
 	while (len--)
 	{
-		if (big[i] == little[i])
+		if (big[i] == little[j])
 		{
-			return (&tmp1[i]);
+			while (little[i + j])
+			{
+				if (little[j])
+				{
+					return ((char *) &big[i]);
+				}
+				j++;
+			}
+
 		}
+		j = 0;
 		i++;
 	}
+
 	return (0);
 }
 
 int main()
 {
-	const char *largestring = "Foo Bar Baz";
+	const char *largestring = "Foo B Bar Baz";
 	const char *smallstring = "Bar";
-	char	*ptr;
+	char	*ptr1;
+	char	*ptr2;
 
-	ptr = ft_strnstr(largestring, smallstring, 7);
-	printf("%s", ptr);
+	ptr1 = strnstr(largestring, smallstring, 9);
+	ptr2 = ft_strnstr(largestring, smallstring, 9);
+	printf("Orginal : %s\n", ptr1);
+	printf("Fake : %s\n", ptr2);
 }
