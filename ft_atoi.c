a62fd42ab96cf572c8d0806540be1391b633114f
Fritz Gabler <fgabler@1-A-4.42heilbronn.de>
2023-03-21 12:47:04 +0100

    version06

---
+++ ft_atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <fgabler@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 11:42:56 by fgabler           #+#    #+#             */
+/*   Updated: 2023/03/21 12:45:12 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	neg;
+	int	res;
+
+	i = 0;
+	res = 0;
+	neg = 1;
+	if (ft_isalpha(*str))
+	{
+		return (0);
+	}
+	if (str[0] == '-')
+	{
+		neg = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		res = res * 10 + str[i] - 48;
+	}
+	return (res * neg);
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+   int val;
+   char str[20];
+
+   strcpy(str, "98993489");
+   val = ft_atoi(str);
+   printf("String value = %s, Int value = %d\n", str, val);
+
+   strcpy(str, "tutorialspoint.com");
+   val = ft_atoi(str);
+   printf("String value = %s, Int value = %d\n", str, val);
+
+   return(0);
+}
+
