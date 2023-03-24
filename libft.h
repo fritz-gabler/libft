1b890ecffde6a58d463bc970d9f879a2aeedbc48
Fritz Gabler <fgabler@1-A-6.42heilbronn.de>
2023-03-24 19:46:49 +0100

    version08

--- libft.h
+++ libft.h
@@ -6,25 +6,35 @@
 /*   By: fgabler <fgabler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 17:56:04 by fgabler           #+#    #+#             */
-/*   Updated: 2023/03/20 10:32:02 by fgabler          ###   ########.fr       */
+/*   Updated: 2023/03/24 18:13:30 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
+# ifndef LIBFT_H
 # define LIBFT_H
 
-void ft_bzero(void *s, size_t n);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isdigit(int c);
-int ft_isprint(int c);
-void *ft_memcpy(void *dst, const void *src, size_t n);
-void *ft_memmove(void *dst, const void *src, size_t len);
-void *ft_memset(void *b, int c, size_t len);
-size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t ft_strlen(const char *str);
-int ft_strncmp(const char *str1, const char *str2, size_t n);
-int toupper(int ch);
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h> // remove!!
+
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *str);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
+int		ft_toupper(int ch);
+int		ft_isalpha(int c);
+void	*ft_calloc(size_t nitems, size_t size);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *str1);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
