de30cdb23c3af75ccb32d748847c5a1352782f21
Fritz Gabler <fgabler@1-A-8.42heilbronn.de>
2023-04-11 20:13:29 +0200

    version16

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fgabler <fgabler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 10:03:45 by fgabler           #+#    #+#             */
-/*   Updated: 2023/04/11 18:04:40 by fgabler          ###   ########.fr       */
+/*   Updated: 2023/04/11 20:11:44 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	unsigned int	i;
 	unsigned int	j;
 	char			*new;
-	unsigned int	len;
 
 	i = 0;
 	j = 0;
-	len = ft_strlen(s1) + ft_strlen(s2);
-	new = ft_calloc(len + 1, sizeof (int));
+	if (!s1 && !s2)
+		return (NULL);
+	new = (char *)ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	if (!new)
-		return (0);
+		return (NULL);
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (ft_strdup(""));
 	while (s1[i])
@@ -33,10 +33,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		i++;
 	}
 	while (s2[j])
-	{
-		new[i] = s2[j];
-		j++;
-		i++;
-	}
+		new[i++] = s2[j++];
+	new = 
 	return (new);
 }
